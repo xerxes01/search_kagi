bb6dd2835553ad07e93e3a7b2df730d01155a08e
shikhar seth <shikharseth15@gmail.com>
2020-07-18 11:17:14 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Deploying on local machine
+clone the repository
+set terminal working directory as cloned repository
+
+# Build instructions
+
+```bash
+sudo docker build -t my_docker_flask:latest .
+```
+
+# Run instructions 
+
+```bash
+sudo docker run -p 5000:5000 my_docker_flask:latest 
+```
+
+# Check installation
+
+```bash
+sudo docker ps
+```
+# Working command 
+```bash
+http://0.0.0.0:5000/get_response_form
+```
+
---
+++ app.py
@@ -0,0 +1,133 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+import tensorflow_text
+import nltk
+from IPython.display import HTML, display
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize,sent_tokenize
+from nltk.stem import PorterStemmer,SnowballStemmer
+from rank_bm25 import BM25Plus
+from sklearn.metrics.pairwise import cosine_similarity
+from flask import Flask, request
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+stop_words = set(stopwords.words('english'))
+
+
+ps = PorterStemmer()
+sn = SnowballStemmer('english')
+
+
+
+module = hub.load('https://tfhub.dev/google/universal-sentence-encoder-qa/3')
+
+
+def angular_similarity(x,y):
+  return (1 - (np.arccos(cosine_similarity(x, y)) / np.pi))
+
+
+def preprocess(text):
+  word_tokens = word_tokenize(text)
+  filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+
+  stemmed_sentence = [sn.stem(w) for w in filtered_sentence]
+  # stemmed_sentence = [ps.stem(w) for w in filtered_sentence]
+    
+  text = " ".join(filtered_sentence)
+
+  return text
+
+
+def return_top_n(zipped,n=3):
+  x =  [r for s,r in sorted(zipped, key = lambda x: x[0])[::-1][:n]]
+  return x
+  
+
+app = Flask(__name__)
+
+
+
+@app.route('/get_response_form',methods=['GET','POST'])
+def get_resp():
+
+	if request.method == 'POST':         	
+		
+		sent = request.form["sent"]
+		query = request.form["query"]
+		weight1 = request.form["weight1"]
+		weight2 = request.form["weight2"]
+		
+
+		
+		######
+		if weight1 == "":
+			weight1 = 0.5
+		else:
+			weight1 = float(weight1)
+
+		if weight2 == "":
+			weight2 = 0.5
+		else:
+			weight2 = float(weight2)
+
+
+		######
+		sentences = sent.split(',')
+
+		preprocessed_query = preprocess(query)
+		preprocessed_sentences = [preprocess(sent) for sent in sentences]
+
+		candidate_embeddings = module.signatures['response_encoder'](
+	    	input=tf.constant(preprocessed_sentences),
+	    	context=tf.constant(preprocessed_sentences))['outputs']
+
+		query_embedding = module.signatures['question_encoder'](tf.constant([preprocessed_query]))['outputs'][0]
+		similarities = angular_similarity([query_embedding],candidate_embeddings)
+		score = similarities[0]   ###########  Ranker 1 - USEQA Angular #########
+
+
+
+
+	#### BM25+ ####
+
+		tokenized_corpus = [doc.split(" ") for doc in preprocessed_sentences]
+		ranker = BM25Plus(tokenized_corpus)
+
+		tokenized_query = preprocessed_query.split(" ")
+
+		r2 = ranker.get_scores(tokenized_query)
+
+		w1 = weight1
+		w2 = weight2
+
+		print("######################")
+		print("weight one : " , w1)
+		print("weight two : ", w2)
+		print("####################")
+
+
+		weighted_final = w1*score + w2*r2
+
+		zipped = list(zip(weighted_final,sentences))
+
+		resp1 = return_top_n(zipped)
+		
+		final = {"Query" : query,
+			"result" : resp1}
+		
+		return(final)
+	
+	return '''<form method="POST">
+                  	Query: <input type="text" name="query" size='50'><br><br>
+			<label for="Candidates">Candidates:</label>
+			<textarea id = "Candidates" rows="20" cols ="100" name="sent" placeholder="Enter all candidate strings here.."></textarea><br><br>
+                  	USEQA_weight: <input type="number" placeholder="0.5" step="0.01" min="0" max="10" name="weight1"><br>
+                  	BM25+_weight: <input type="number" placeholder="0.5" step="0.01" min="0" max="10" name="weight2"><br>
+                  	<input type="submit" value="Submit"><br>
+                  </form>'''
+
+if __name__ == '__main__':
+   app.run(host= '0.0.0.0',debug = True)
